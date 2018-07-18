9a2b9de7a291ff1226c379a910d83c44c98f99ad
sriharsha67 <sriharsha67@gmail.com>
2018-07-18 16:20:03 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*General*/
+/*$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});*/
+
+// jQuery(document).ready(function($) {
+//   function subscribe()
+// {
+// var val=document.getElementById('becomePartner').value;
+
+// $.post('../subscribe.php',{email:val},function(d){
+// if(d==200)
+// {
+// alert('Thank you! We will get back to you soon.');    
+// }
+// else
+// {
+// alert('Please try again later.');        
+// }
+// });
+
+// alert(val);
+// }
+// });
+/*General*/
